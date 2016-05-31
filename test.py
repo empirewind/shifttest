b66ac1b01b4f410b4118fe4e36ed27cdc2e43d61
empire_wind@163.com <61816896402589s>
2016-05-31 17:59:11 +0800

    提交test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+s=input();
+
+f=open("b:\gittest\test.txt
+       ")
+       while True:
+       line=f.read()
+       if line:
+       pass
+       line =line.strip()
+       p=line.rfind('.')
+       filename=line[0:p]
+       print "create %s" %line
+    else:
+       break;
+f.close()
+
+       s=input();
+
+
+
+
+        
+
+ 
